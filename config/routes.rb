835bc3b7362bb494a3115eb1dfe90706641f8fea
Oleksandr Papka <papkaos0924@gmail.com>
2017-08-13 14:18:12 +0300

    add destroy users

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ MarketPlaceApi::Application.routes.draw do
   namespace :api, defaults: { format: :json } do
     scope module: :v1 do
 
-      resources :users, :only => [:show, :create, :update]
+      resources :users, :only => [:show, :create, :update, :destroy]
     end
   end
 
